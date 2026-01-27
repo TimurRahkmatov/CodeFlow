fcd25445e398b51caebec4c168069e0d082ec9a7
Timur Raxmatov <raxmatovtimurjava@gmail.com>
2026-01-28 02:09:03 +0500

    added code card to Hero section

---
+++ src/data/CodeExamples.js
@@ -0,0 +1,65 @@
+export const CodeExamples = {
+    "App.jsx" : `
+    import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white overflow-hidden ">
+      <Navbar />
+      <Hero />
+      <Testimonials />
+      <Pricing />
+      <Footer />
+    </div>
+  );
+}
+
+export default App; 
+    `,
+    "Hero.jsx" : `
+    import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+
+function Hero() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white overflow-hidden ">
+      <Navbar />
+      <Hero />
+      <Testimonials />
+      <Pricing />
+      <Footer />
+    </div>
+  );
+}
+
+export default Hero; 
+    `,
+    "Navbar.jsx" : `
+    import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+
+function Navbar() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white overflow-hidden ">
+      <Navbar />
+      <Hero />
+      <Testimonials />
+      <Pricing />
+      <Footer />
+    </div>
+  );
+}
+
+export default Navbar; 
+    `
+}
